a0c53b2a1cdfa2cf7b3cac8c7cf236818b902062
cesarabad <cesarabadvel@gmail.com>
2025-04-12 20:40:25 +0200

    refact(MuffinShapePage): Refactoring page to use generic components

--- src/pages/pravite/manage-data/shape-management/shape-management.tsx
+++ src/pages/pravite/manage-data/shape-management/shape-management.tsx
@@ -1,23 +1,47 @@
+import { useTranslation } from "react-i18next";
 import { CrudManagerPage } from "../../../../components/app/generic-crud-manager-page/generic-crud-manager-page.component";
 import { MuffinShapeDto } from "../../../../models/muffin-shape/muffin-shape-dto.model";
 import { muffinShapeService } from "../../../../services/manage-data/muffin-shape.service";
-import { Input } from "antd";
+import { GenericInput } from "../../../../components/app/generic-form/generic-input.component";
 
 export default function MuffinShapeCrudPage() {
+  const { t } = useTranslation();
   return (
     <CrudManagerPage<MuffinShapeDto>
-      title="Gestión de Formas de Magdalenas"
+      title={t('manageData.muffinShape.page.title')}
       service={muffinShapeService}
       createEmptyItem={() => ({ reference: "", description: "" })}
       extraColumns={[
-        { title: "Descripción", dataIndex: "description", key: "description" },
+        { title: t('manageData.muffinShape.page.reference.label'), dataIndex: "reference", key: "reference" },
+        { title: t('manageData.muffinShape.page.description.label'), dataIndex: "description", key: "description" },
+        { title: t('manageData.lastModifyDate'), dataIndex: "lastModifyDate", key: "lastModifyDate" },
+        { 
+          title: t('manageData.lastModifyUser'), 
+          dataIndex: ["lastModifyUser", "id"],  
+          key: "lastModifyUser",
+          render: (_: unknown, record: MuffinShapeDto) => `${record.lastModifyUser?.name || ""} ${record.lastModifyUser?.secondName || ""}`.trim()
+        },
       ]}
       renderExtraFields={(item, handleChange) => (
-        <Input
-          placeholder="Descripción"
-          value={item.description || ""}
-          onChange={(e) => handleChange("description", e.target.value)}
-        />
+        <>
+          <GenericInput
+            label={t('manageData.muffinShape.page.reference.label')}
+            placeholder={t('manageData.muffinShape.page.reference.placeholder')}
+            value={item?.reference || ""}
+            onChange={(e) => handleChange("reference", e.target.value)}
+            required={true}
+            maxLength={10}
+            pattern="^[a-zA-Z0-9_ ]*$" // Solo letras, números y guiones bajos
+          />
+          <GenericInput
+            label={t('manageData.muffinShape.page.description.label')}
+            placeholder={t('manageData.muffinShape.page.description.placeholder')}
+            value={item.description || ""}
+            onChange={(e) => handleChange("description", e.target.value)}
+            required={true}
+            maxLength={80}
+          />
+        </>
       )}
     />
   );
